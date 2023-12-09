8a798965af53c204d6d4dda445030d147482ca12
YYBer <liyuanying225@gmail.com>
2023-12-09 15:56:48 +0100

    Day8-2 working

--- Day8/Day8-2.cpp
+++ Day8/Day8-2.cpp
@@ -35,27 +35,45 @@ std::string moveAction(char c, std::string name, std::map<std::string, Token> ma
     return NULL;
 }
 
-void moveSum(std::string instruction, std::map<std::string, Token> map, int& sum, bool start,std::string check)
+void moveSum(std::string instruction, std::map<std::string, Token> map, int& sum, std::string check)
 {
     for (int i = 0; i < instruction.size(); ++i)
     {
-        if(i == 0 && start == true)
-            check = moveAction(instruction[i], "AAA", map);
-        else
-            check = moveAction(instruction[i], check, map);
-        start = false;
+        check = moveAction(instruction[i], check, map);
         if (check != "ZZZ")
             sum++;
         else return;
     }
     if (check != "ZZZ")
-        moveSum(instruction, map, sum, start, check);
+        moveSum(instruction, map, sum, check);
 }
 
+void moveFrame(std::string instruction, std::map<std::string, Token> map)
+{
+    std::map<std::string, Token>::iterator it;
+
+}
+
+int moveRoutine(std::string instruction, std::map<std::string, Token> map, std::map<std::string, Token>::iterator it)
+{
+    while (it != map.end())
+    {
+        if (it->first[2] == 'A')
+        {
+            int sum = 1;
+            moveSum(instruction, map, sum, it->first);
+            return sum;
+        }
+        ++it;
+    }
+
+
+}
 
 int main()
 {
-    std::ifstream ifs ("input.txt");
+    // std::ifstream ifs ("input.txt");
+    std::ifstream ifs ("test3.txt");
     std::string line;
     std::getline(ifs, line);
     std::string instruction = line;
@@ -64,18 +82,18 @@ int main()
     while(std::getline(ifs, line))
     {
         std::string name = line.substr(0, 3);
-        std::string pair = line.substr(line.find('(') + 1, line.find(')'));
-        std::string left = pair.substr(0, pair.find(','));
-        std::string righttmp = pair.substr(5);
-        std::string right = righttmp.substr(0, righttmp.find(')'));
+        size_t openPos = line.find('(');
+        size_t closePos = line.find(')');
+        std::string left = line.substr(openPos + 1, line.find(',') - openPos - 1);
+        std::string right = line.substr(line.find(',') + 1, closePos - line.find(',') - 1);
         Token toc = {left, right};
         map.insert(std::make_pair(name, toc));
     }
-    // printMap(map);
-    int sum = 1;
-    bool start = true;
-    std::string check;
-    moveSum(instruction, map, sum, start, check);
+    printMap(map);
+    // int sum = 1;
+    // bool start = true;
+    // std::string check;
+    // moveSum(instruction, map, sum, start, check);
     std::cout << "sum: "<< sum << std::endl;
     ifs.close();
 }
